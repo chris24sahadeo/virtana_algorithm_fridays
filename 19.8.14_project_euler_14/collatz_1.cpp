d7dad7daf7e7a0a5c0d5d75b0b483164602535e0
Christopher Sahadeo <christophersahadeo@gmail.com>
2019-08-14 18:38:13 -0400

    added collatz solution
    it works but sucks

---
+++ 19.8.14_project_euler_14/collatz_1.cpp
@@ -0,0 +1,65 @@
+// https://projecteuler.net/problem=14
+// https://en.wikipedia.org/wiki/Collatz_conjecture
+// https://www.dcode.fr/collatz-conjecture
+
+#include <iostream>
+#include <sstream>
+#include <map>
+
+#define MAX 1000000
+
+std::map <unsigned int, unsigned int> progression_steps;
+
+unsigned int collatz(unsigned int n)
+{
+  // if n is even
+  if(n % 2 == 0) return n/2;
+
+  // else if n odd
+  return 3*n + 1;
+}
+
+unsigned int getNumSteps(unsigned int n)
+{
+  // base case
+  if(n==1) return 0;
+
+  // if getNumSteps(n) was already found and stored in the map
+  std::map <unsigned int, unsigned int>::iterator itr = progression_steps.find(n);
+  if(itr != progression_steps.end()) return itr->second;
+
+  // if not found in the map
+  unsigned int num_steps = getNumSteps(collatz(n)) + 1;
+  progression_steps[n] = num_steps;
+  return num_steps;
+}
+
+void longestProgression(unsigned int max)
+{
+  unsigned int longest_progression = 0, starting_number;
+  for(unsigned int i = 1; i <= max; i++)
+  {
+    // std::cout << i << std::endl; 
+    int num_steps = getNumSteps(i);
+    
+    if(num_steps > longest_progression)
+    {
+      longest_progression = num_steps;
+      starting_number = i;
+    }
+    
+  }
+
+  std::cout << "Starting number: "<< starting_number << ". Longest progression: " << longest_progression << std::endl;
+}
+
+int main(int argc, char **argv)
+{
+  std::istringstream ss(argv[1]);
+  int n;
+  ss >> n;
+ 
+  longestProgression(n);
+
+  return 0;
+}
